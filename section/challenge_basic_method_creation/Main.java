85ab6c0e332c6efa76535ec53d485150a7505070
2ofdee <>
2026-06-21 18:40:25 +0000

    Challenge: Basic Method Creation

---
+++ section/challenge_basic_method_creation/Main.java
@@ -0,0 +1,21 @@
+package com.example;
+
+public class Main {
+    public static void printWelcome() {
+       System.out.println("Welcome to the Java Methods Challenge!");
+    }
+
+    public static void showInstructions() {
+        System.out.println("Follow the instructions carefully to complete the task.");
+    }
+
+    public static void printCompletion() {
+        System.out.println("Congratulations! You have completed the challenge.");
+    }
+
+    public static void main(String[] args) {
+        printWelcome();
+        showInstructions();
+        printCompletion();
+    }
+}
